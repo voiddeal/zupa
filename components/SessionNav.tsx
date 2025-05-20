0149eac968855fef187ea16d4301ea8b0594abf3
voiddeal <hr.shafiei.dev@gmail.com>
2025-05-21 01:03:54 +0330

    Add SessionNav

--- components/SessionNav.tsx
+++ components/SessionNav.tsx
@@ -6,43 +6,40 @@ import { sessionStatsActions } from "@/redux/slices/sessionStatsSlice"
 import Image from "next/image"
 import Portal from "./Portal"
 import Records from "./Records"
-import unFlip from "@/utils/unFlipTiles"
+import Settings from "./settings/Settings"
+import unFlipTiles from "@/utils/unFlipTiles"
 
 interface SessionNavProps {
   board: React.RefObject<HTMLDivElement | null>
-  isAnyTileOpen: React.RefObject<boolean>
-  OpenTileID: React.RefObject<string | null>
 }
 
-export default function SessionNav({
-  board,
-  OpenTileID,
-  isAnyTileOpen,
-}: SessionNavProps) {
-  const { recordsDisplay } = useAppSelector((state) => state.app)
+export default function SessionNav({ board }: SessionNavProps) {
   const dispatch = useAppDispatch()
+  const { recordsDisplay, settingsDisplay } = useAppSelector(
+    (state) => state.app
+  )
   const NAV_BUTTON_CLASSNAMES =
     "size-10 rounded-full bg-yellow-200 overflow-hidden cursor-pointer hover:scale-110 active:scale-100 transition-transform"
 
   const restart = () => {
-    resetSessionStats()
-    unFlip({ board, all: true, wait: false })
-    OpenTileID.current = null
-    isAnyTileOpen.current = false
-  }
-
-  const resetSessionStats = () => {
     dispatch(sessionStatsActions.reset())
+    dispatch(sessionStatsActions.clearActiveTileID())
+    unFlipTiles({ board: board, all: true, shouldWait: false })
   }
 
   const showRecords = () => {
     dispatch(appActions.setRecordsDisplay(true))
   }
 
+  const showSettings = () => {
+    dispatch(appActions.setIsTimerPaused(true))
+    dispatch(appActions.setSettingsDisplay(true))
+  }
+
   return (
-    <nav className="absolute bottom-6">
+    <nav className="absolute bottom-6 max-md:bottom-2">
       <ul className="flex gap-x-5 bg-orange-200 rounded-xl px-4 py-2">
-        <li className={NAV_BUTTON_CLASSNAMES}>
+        <li className={NAV_BUTTON_CLASSNAMES} onClick={showSettings}>
           <Image
             src={"/menu-button.png"}
             alt="menu button"
@@ -59,11 +56,7 @@ export default function SessionNav({
             className="scale-110"
           />
         </li>
-        <li
-          className={NAV_BUTTON_CLASSNAMES}
-          onClick={restart}
-          title="Resetart"
-        >
+        <li className={NAV_BUTTON_CLASSNAMES} onClick={restart} title="Restart">
           <Image
             src={"/restart-button.png"}
             alt="restart button"
@@ -73,9 +66,16 @@ export default function SessionNav({
           />
         </li>
       </ul>
+
       {recordsDisplay && (
         <Portal>
           <Records />
+        </Portal>
+      )}
+
+      {settingsDisplay && (
+        <Portal>
+          <Settings board={board} />
         </Portal>
       )}
     </nav>
