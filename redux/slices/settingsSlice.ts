933fcbcbdf14c178a420ff46cb9a9d0e0100ad15
voiddeal <hr.shafiei.dev@gmail.com>
2025-05-21 00:50:52 +0330

    Add settingsSlice

---
+++ redux/slices/settingsSlice.ts
@@ -0,0 +1,71 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { RootState } from "../store"
+import type { Settings } from "@/types/models"
+
+const settings =
+  typeof window !== "undefined" ? localStorage.getItem("settings") : null
+
+const initialState: Settings = settings
+  ? JSON.parse(settings)
+  : {
+      boardTileCount: 16,
+      sessionTime: 60,
+      tileUnFlipDelay: 1500,
+      tileMode: "animals",
+      shouldCalculateMistakes: false,
+      shouldCalculateAttempts: true,
+      sounds: true,
+    }
+
+const settingsSlice = createSlice({
+  name: "settings",
+  initialState,
+  reducers: {
+    setTileFlipDelay: (
+      state,
+      actions: PayloadAction<Settings["tileUnFlipDelay"]>
+    ) => {
+      state.tileUnFlipDelay = actions.payload
+    },
+
+    setSessionTime: (
+      state,
+      actions: PayloadAction<Settings["sessionTime"]>
+    ) => {
+      state.sessionTime = actions.payload
+    },
+
+    setBoardTileCount: (
+      state,
+      actions: PayloadAction<Settings["boardTileCount"]>
+    ) => {
+      state.boardTileCount = actions.payload
+    },
+
+    setTileMode: (state, actions: PayloadAction<Settings["tileMode"]>) => {
+      state.tileMode = actions.payload
+    },
+
+    setShouldCalculateMistakes: (
+      state,
+      action: PayloadAction<Settings["shouldCalculateMistakes"]>
+    ) => {
+      state.shouldCalculateMistakes = action.payload
+    },
+
+    setShouldCalculateAttempts: (
+      state,
+      action: PayloadAction<Settings["shouldCalculateAttempts"]>
+    ) => {
+      state.shouldCalculateAttempts = action.payload
+    },
+
+    setSounds: (state, action: PayloadAction<Settings["sounds"]>) => {
+      state.sounds = action.payload
+    },
+  },
+})
+
+export default settingsSlice.reducer
+export const settingsActions = settingsSlice.actions
+export const selectRecord = (state: RootState) => state.settings
---
+++ types/models.ts
@@ -0,0 +1,9 @@
+export interface Settings {
+  tileUnFlipDelay: 1000 | 1500 | 2000
+  boardTileCount: 16 | 36
+  tileMode: "animals" | "flowers" | "humans"
+  sessionTime: 60 | 90 | 120
+  shouldCalculateMistakes: boolean
+  shouldCalculateAttempts: boolean
+  sounds: boolean
+}
